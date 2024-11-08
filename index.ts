9df7b93b801fcd9953b37b0ad37bed4d70967c65
Damien <80228708+damienbullis@users.noreply.github.com>
2024-11-08 16:58:54 -0600

    testing out function version

--- index.ts
+++ index.ts
@@ -1,3 +1,40 @@
-const test = 'Hello via Bun!'
+/**
+ * Hexin Engine
+ *
+ * A simple modular game engine, using TypeScript.
+ */
+interface System {
+    name: string
+}
+interface OtherSystem extends System {}
+interface AnotherSystem extends System {}
+function Hexin<
+    T extends {
+        [K in keyof T]: T[K] extends System ? T[K] : never
+    },
+>(options: T): T {
+    return options
+}
+
+function wrong(): System {
+    return {
+        name: 'wrong',
+    }
+}
+class s1 implements System {
+    name = 's1'
+}
+class s2 implements OtherSystem {
+    name = 's2'
+}
+class s3 implements AnotherSystem {
+    name = 's3'
+}
+
+const engine = Hexin({
+    system1: new s1(),
+    system2: new s2(),
+    system3: wrong(),
+})
 
 console.log('Hello via Bun!')
