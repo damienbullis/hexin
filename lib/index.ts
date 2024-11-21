617d716529fa1bd7ed34658b1fa9434a02831105
Damien <80228708+damienbullis@users.noreply.github.com>
2024-11-21 08:26:00 -0600

    add index

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+import { makeEngine } from './engine'
+import { makeComponent } from './entities'
+import { makeSystem } from './systems'
+import type { Engine, System, Component } from './types'
+
+export { makeEngine, makeComponent, makeSystem }
+export type { Engine, System, Component }
