fc64e4be6c06ac7ad31314b5ce3e6a02d0d18659
Damien <80228708+damienbullis@users.noreply.github.com>
2024-11-27 07:41:13 -0600

    update engine test

--- lib/engine.test.ts
+++ lib/engine.test.ts
@@ -1,8 +1,26 @@
-import { equal } from 'assert'
-import { describe, it } from 'bun:test'
+import { describe, it, expect } from 'bun:test'
+import { Hex } from './engine'
+import { LogLevel, MemoryLog, type LoggerOptions } from './log'
 
 describe('core', () => {
-    it('should be equal', () => {
-        equal(1, 1)
+    it('should initialize with no config', () => {
+        const hex = new Hex()
+        const options = hex.utils.config.log_options
+
+        expect(options?.level).toBe(LogLevel.DEBUG)
+        expect(options?.outputs).toHaveLength(1)
+    })
+    it('should initialize with a config', () => {
+        const mem = new MemoryLog()
+        const log_options: LoggerOptions = {
+            level: LogLevel.DEBUG,
+            outputs: [mem],
+        }
+        const hex = new Hex({ log_options })
+        const options = hex.utils.config.log_options
+
+        expect(options?.level).toBe(LogLevel.DEBUG)
+        expect(options?.outputs).toHaveLength(1)
+        expect(mem.print().length).toBeGreaterThan(0)
     })
 })
