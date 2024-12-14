9a47d0c6f907c1d388c324b6305ea66e1ba26497
Damien <80228708+damienbullis@users.noreply.github.com>
2024-12-14 08:39:40 -0600

    refactor: remove unused assertion functions and simplify color handling in rainbowIntroText

--- utils.ts
+++ utils.ts
@@ -1,27 +1,3 @@
-/**
- * Asserts that the condition is true, otherwise throws an error with the message
- * @param condition If the condition is false, an error will be thrown with the message
- * @param message The message to throw if the condition is false
- */
-export function assert<T>(
-    condition: unknown,
-    message: string
-): asserts condition is T {
-    if (!condition) {
-        throw new Error(message)
-    }
-}
-
-export function assertNever(x: never): never {
-    throw new Error('Unexpected object: ' + x)
-}
-
-/**
- * Warning this function will dangerously cast the type of the value to the type T
- * @param _ The value to assert
- */
-export function assertType<T>(_: unknown): asserts _ is T {}
-
 export const fg = (color: string, text: string) =>
     `\x1b[38;2;${Bun.color(color, 'ansi')}${text}\x1b[0m`
 
@@ -47,11 +23,8 @@ export function rainbowIntroText() {
             const divs = line.length / rainbow.length
             const offset = 3 - x
             const color =
-                rainbow[
-                    Math.floor((i - offset < 0 ? i : i - offset) / divs) %
-                        rainbow.length
-                ]
-            assert<string>(color, 'Color should exist')
+                rainbow[Math.floor((i - offset < 0 ? i : i - offset) / divs) % rainbow.length]
+            if (!color) return char
             return fg(color, char)
         })
     )
